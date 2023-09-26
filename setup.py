b88032f24726ff3f9e7322b05a936938b2153c60
Eriq Augustine <eriq.augustine@gmail.com>
2023-09-26 12:39:07 -0500

    Updated version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     name = 'autograder-py',
     url = 'https://github.com/eriq-augustine/autograder-py',
 
-    version = '0.1.3',
+    version = '0.2.0',
     keywords = 'grading',
 
     description = "The Python interface for the autograding server.",
