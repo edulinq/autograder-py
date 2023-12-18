3aba939cc2d81f58e0a21ce0dbb573c1b7ba8746
Oliver <oliver.lokhandwala@gmail.com>
2023-12-17 20:43:14 -0800

    pushing to test CI on github

--- autograder/api/submission/remove.py
+++ autograder/api/submission/remove.py
@@ -1,7 +1,7 @@
 import autograder.api.common
 import autograder.api.config
 
-API_ENDPOINT = 'submission/remove/submission'
+API_ENDPOINT = 'submission/remove'
 API_PARAMS = [
     autograder.api.config.PARAM_COURSE_ID,
     autograder.api.config.PARAM_USER_EMAIL,
