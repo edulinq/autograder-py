b8c40fc0c066662bf9838e30ea80d7b7795738c5
Eriq Augustine <eriq.augustine@gmail.com>
2024-01-02 20:44:51 +0700

    Upped vesion.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     name = 'autograder-py',
     url = 'https://github.com/eriq-augustine/autograder-py',
 
-    version = '0.3.2',
+    version = '0.4.0',
     keywords = 'grading',
 
     description = "The Python interface for the autograding server.",
