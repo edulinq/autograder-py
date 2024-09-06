171eb78093ecc4a2cae9519af9470ea2432c6eb4
Eriq Augustine <eriq.augustine@gmail.com>
2024-09-06 10:42:56 -0700

    Added an exception for numpy when reloading modules for test submissions.

--- autograder/submission.py
+++ autograder/submission.py
@@ -160,6 +160,12 @@ def run_test_submission(assignment_config_path, submission_config_path, debug = 
     finally:
         new_module_keys = set(sys.modules.keys())
         for new_module_key in (new_module_keys - old_module_keys):
+            # Numpy is a special case that iv very delicate to reloads.
+            # Note that this would be a security concern (a submission hijacking numpy),
+            # but this is not used in docker-based grading.
+            if (new_module_key.startswith('numpy')):
+                continue
+
             if (new_module_key in sys.modules):
                 del sys.modules[new_module_key]
 
