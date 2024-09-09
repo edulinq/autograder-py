838b271dbe31738369a032a312930b7c6d1a4fef
Lucas Ellenberger <121155755+Lucas-Ellenberger@users.noreply.github.com>
2024-09-09 16:04:22 -0700

    Moved the API endpoint for updating a course to its new location. (#24)

--- autograder/cli/courses/admin/update.py
+++ autograder/cli/courses/admin/update.py
@@ -1,9 +1,9 @@
 import sys
 
-import autograder.api.admin.updatecourse
+import autograder.api.courses.admin.update
 
 def run(arguments):
-    result = autograder.api.admin.updatecourse.send(arguments, exit_on_error = True)
+    result = autograder.api.courses.admin.update.send(arguments, exit_on_error = True)
 
     if (result['course-updated']):
         print("Course updated.")
@@ -18,7 +18,7 @@ def main():
     return run(_get_parser().parse_args())
 
 def _get_parser():
-    return autograder.api.admin.updatecourse._get_parser()
+    return autograder.api.courses.admin.update._get_parser()
 
 if (__name__ == '__main__'):
     sys.exit(main())
