0ec8573ae155013b9202eee3f04a26078c791e00
Lucas Ellenberger <lmellenb@ucsc.edu>
2024-09-19 13:07:54 -0700

    Added support for the courses/users/drop API endpoint.

---
+++ autograder/cli/courses/users/__main__.py
@@ -0,0 +1,13 @@
+"""
+The `autograder.cli.courses.users` package contains tools to manage autograder course users.
+"""
+
+import sys
+
+import autograder.util.cli
+
+def main():
+    return autograder.util.cli.main()
+
+if (__name__ == '__main__'):
+    sys.exit(main())
