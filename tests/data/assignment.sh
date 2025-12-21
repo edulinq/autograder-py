d8462f15b91088ad138801ed73a7ba32e67aa72a
Eriq Augustine <eriq.augustine@gmail.com>
2025-12-21 13:20:18 -0800

    Removed old API/CLI tests. They will be slowly added back as we refactor, but will be updated for the new infrastructure.

--- tests/data/assignment.sh
+++
@@ -1,5 +0,0 @@
-function add() {
-    local a=$1
-    local b=$2
-    echo $((a + b))
-}
--- tests/data/hw0_no_compile.py
+++
@@ -1,6 +0,0 @@
-# Missing colon.
-def function1()
-    return True
-
-def function2(val):
-    return val + 1
