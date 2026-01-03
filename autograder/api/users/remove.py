6a3809c935d68bb4959fff300ad0fc03d26fb06f
Eriq Augustine <eriq.augustine@gmail.com>
2026-01-02 19:58:55 -0800

    Added support for users/remove.

---
+++ autograder/api/users/remove.py
@@ -0,0 +1,23 @@
+"""
+Remove a user from the server.
+"""
+
+import typing
+
+import autograder.api.common
+import autograder.api.config
+
+API_ENDPOINT: str = 'users/remove'
+API_WRITE: bool = True
+API_PARAMS: typing.List[autograder.api.config.APIParam] = [
+    autograder.api.config.PARAM_SERVER,
+    autograder.api.config.PARAM_USER_EMAIL,
+    autograder.api.config.PARAM_USER_PASS,
+
+    autograder.api.config.PARAM_TARGET_EMAIL,
+]
+
+def send(config: typing.Dict[str, typing.Any], **kwargs: typing.Any) -> typing.Dict[str, typing.Any]:
+    """ Send a request to the autograder. """
+
+    return autograder.api.common.make_api_request(API_ENDPOINT, config, API_PARAMS, write = API_WRITE, **kwargs)
