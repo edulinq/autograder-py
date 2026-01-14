6f8a3b858bd6331779f3f365bf4c8f81acf5f3f9
Eriq Augustine <eriq.augustine@gmail.com>
2026-01-14 09:57:35 -1000

    Added support for courses/assignments/submissions/fetch/courses/attempts.

---
+++ autograder/api/courses/assignments/submissions/fetch/course/attempts.py
@@ -0,0 +1,33 @@
+"""
+Get all recent submissions and grading information for this assignment.
+"""
+
+import typing
+
+import autograder.api.common
+import autograder.api.config
+import autograder.assignment
+
+API_ENDPOINT: str = 'courses/assignments/submissions/fetch/course/attempts'
+API_WRITE: bool = False
+API_PARAMS: typing.List[autograder.api.config.APIParam] = [
+    autograder.api.config.PARAM_SERVER,
+    autograder.api.config.PARAM_USER_EMAIL,
+    autograder.api.config.PARAM_USER_PASS,
+
+    autograder.api.config.PARAM_COURSE,
+    autograder.api.config.PARAM_ASSIGNMENT,
+
+    autograder.api.config.PARAM_COURSE_USER_REFERENCES,
+
+    autograder.api.config.PARAM_OUT_DIR,
+]
+
+def send(config: typing.Dict[str, typing.Any], **kwargs: typing.Any) -> typing.Dict[str, typing.Dict[str, typing.Any]]:
+    """
+    Send a request to the autograder.
+    Return a map of grading results keyed by user email.
+    """
+
+    response = autograder.api.common.make_api_request(API_ENDPOINT, config, API_PARAMS, write = API_WRITE, **kwargs)
+    return response['grading-results']
---
+++ autograder/cli/courses/assignments/submissions/fetch/course/attempts.py
@@ -0,0 +1,46 @@
+"""
+Get all recent submissions and grading information for this assignment.
+"""
+
+import argparse
+import sys
+
+import autograder.api.courses.assignments.submissions.fetch.course.attempts
+import autograder.cli.parser
+import autograder.util.submission
+
+def run_cli(args: argparse.Namespace) -> int:
+    """ Run the CLI. """
+
+    config = args._config
+
+    grading_results = autograder.api.courses.assignments.submissions.fetch.course.attempts.send(config)
+
+    count = 0
+    for grading_result in grading_results.values():
+        if (grading_result is None):
+            continue
+
+        autograder.util.submission.output_grading_result(grading_result, base_dir = config['out_dir'])
+        count += 1
+
+    print(f"Wrote {count} attempts to '{config['out_dir']}'.")
+
+    return 0
+
+def main() -> int:
+    """ Get a parser, parse the args, and call run. """
+
+    return run_cli(_get_parser().parse_args())
+
+def _get_parser() -> argparse.ArgumentParser:
+    """ Get a parser for this operation. """
+
+    parser = autograder.cli.parser.get_parser(
+        __doc__.strip(),
+        autograder.api.courses.assignments.submissions.fetch.course.attempts.API_PARAMS)
+
+    return parser
+
+if (__name__ == '__main__'):
+    sys.exit(main())
